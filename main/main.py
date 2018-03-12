24244bde95240bda8ee9617d3eacb7e3f45aa246
Aaron <ledzeppelindrummer@gmail.com>
2018-03-12 18:14:13 -0400

    Now will generate n number of shifts

--- main/main.py
+++ main/main.py
@@ -6,6 +6,10 @@ cypherText = "IUUBL ESBCJ GMANT ODRZC VUITT ALESA BJHGF RNBNT OMBUG HBTTH AUNBN"
 
 strs = string.ascii_uppercase
 
+maxShift = 1
+
+#algorithm based on
+#https://stackoverflow.com/questions/14424500/text-shift-function-in-python
 def shiftBy(shift, inputStr):
     inputStr = inputStr.upper()
     shiftText = ""
@@ -16,21 +20,45 @@ def shiftBy(shift, inputStr):
             shiftText += "" +  char
     return shiftText
 
+def createLabels(parent, shift, inputStr, backwards=False, font='Helvetica 22 bold'):
+    labelList = []
+    if not backwards:
+        for x in range(shift):
+            labelList.append(
+                ttk.Label(parent, text=shiftBy(x+1, inputStr),
+                      font=font))
+    else:
+        for x in range(shift, 0, 1):
+            print x
+            labelList.append(
+                ttk.Label(mainFrame, text=shiftBy(x, inputStr),
+                          font=font))
+
+    return labelList
+
+
 if __name__ == '__main__':
     root = Tk()
     root.title("Shift Cipher Helper")
     mainFrame = ttk.Frame(root)
 
-    mainFrame.grid(column=0, row=2, sticky=(N, W, E, S))
+    mainFrame.grid(column=0, row=(maxShift*2)+1, sticky=(N, W, E, S))
 
+    createLabels(mainFrame, -1 * maxShift, cypherText, backwards=True)
 
-    textShift1 = ttk.Label(mainFrame, text = shiftBy(-1, cypherText),
-                           font='Helvetica 22 bold').grid(column=0, row=0)
     cText = ttk.Label(mainFrame, text=shiftBy(0, cypherText),
-                      font='Helvetica 22 bold').grid(column=0, row=1)
-    textShift2 = ttk.Label(mainFrame, text = shiftBy(1, cypherText),
-                           font='Helvetica 22 bold').grid(column=0, row=2)
+                      font='Helvetica 22 bold')
 
-    #mainFrame.pack()
+
+    createLabels(mainFrame, maxShift, cypherText)
+
+
+
+
+
+    count = 0
+    for child in mainFrame.winfo_children():
+        child.grid(column=0, row=count)
+        count += 1
 
     root.mainloop()
