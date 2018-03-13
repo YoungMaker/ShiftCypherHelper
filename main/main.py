c1c69655cc8bd3ff6cdc3163729dbe71861d78b2
Aaron <ledzeppelindrummer@gmail.com>
2018-03-12 20:26:14 -0400

    Output list now displayed.

--- main/main.py
+++ main/main.py
@@ -1,11 +1,14 @@
 import string
 from tkinter import *
+from tkinter.ttk import Separator, Style
 
 cypherText = "IUUBL ESBCJ GMANT ODRZC VUITT ALESA BJHGF RNBNT OMBUG HBTTH AUNBN"
 
 strs = string.ascii_uppercase #reference string of ascii upercase letters
 
-maxShift = 1
+maxShift = 2
+
+outputVarList = []
 
 #algorithm based on
 #https://stackoverflow.com/questions/14424500/text-shift-function-in-python
@@ -23,9 +26,17 @@ def shiftBy(shift, inputStr):
 def highlight(event):
     if event.widget.cget("fg") == "red":
         event.widget.config(fg="black")
+        col = event.widget.grid_info()["column"]
+        updateRow(outputVarList, int(col), "")
+
     else:
         event.widget.config(fg="red")
+        col = event.widget.grid_info()["column"]
 
+        updateRow(outputVarList, int(col),  event.widget.cget('text'))
+
+def updateRow(labelList, col, text):
+    labelList[int(col)].set(text)
 
 
 def createLabels(parent, shift, inputStr, rowStart, backwards=False, font='Helvetica 22 bold'):
@@ -44,34 +55,46 @@ def createLabels(parent, shift, inputStr, rowStart, backwards=False, font='Helve
     return labelList
 
 def createRow(parent, text, rowNum, font='Helvetica 22 bold'):
-    labelList = []
+    varList = []
     col =0
     for char in text:
         if char in strs:
-            cLabel = Label(parent, text=char,
+            cLabelVar = StringVar()
+            cLabelVar.set(char)
+            cLabel = Label(parent, textvariable=cLabelVar,
                        font=font)
+            varList.append(cLabelVar)
             cLabel.bind("<Button-1>", highlight)
             cLabel.grid(row=rowNum, column=col)
-            labelList.append(cLabel)
+
 
         else:
-            cLabel = Label(parent, text=char,
+            cLabelVar = StringVar()
+            cLabelVar.set(char)
+            Label(parent, textvariable=cLabelVar,
                            font=font).grid(row=rowNum, column=col)
-            labelList.append(cLabel)
+            varList.append(cLabelVar)
 
         col+=1
 
-    return labelList
+    return varList
 
 
 
 
 if __name__ == '__main__':
+    #just used to setup the row.
+    outputStr = ""
+    for char in cypherText:
+        outputStr += " "
+
     root = Tk()
     root.title("Shift Cipher Helper")
+    root.resizable(width=False, height=False)
     mainFrame = Frame(root)
 
-    mainFrame.grid(column=len(cypherText)+1, row=(maxShift*2)+1, sticky=(N, W, E, S))
+    mainFrame.grid(column=0, row=0, sticky=(N, W, E, S))
+
 
 
     createLabels(mainFrame, -1 * maxShift, cypherText, 0, backwards=True)
@@ -79,6 +102,12 @@ if __name__ == '__main__':
     createRow(mainFrame, cypherText,  maxShift+1)
 
     createLabels(mainFrame, maxShift, cypherText, maxShift+2, backwards=False)
+
+    sep = Separator(root, orient="horizontal")
+    sep.grid(column= 1, row =(maxShift*2)+1, sticky="ws")
+
+    outputVarList = createRow(mainFrame, outputStr, (maxShift*2)+2)
+
 
     #add all items to the grid layout in mainFrame
     # count = 0
